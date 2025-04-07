0c5eff430e92b0b9bbfdfa3a1f365f69842cc9f7
Andre Fachat <fachat@web.de>
2025-04-08 00:21:20 +0200

    cleanup FPGA build; separate xise project per PCB, script to create ROMs

---
+++ mkbins.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+. /opt/Xilinx/14.7/ISE_DS/settings64.sh
+
+for i in ultracpu21a ultipet12a micropet30b; do
+
+	if test $i/*.bit -nt $i.bin ; then 
+		echo "make it"
+		promgen -spi -p bin -w -u 0 $i/*.bit -o $i.bin
+	fi;
+done;
+
